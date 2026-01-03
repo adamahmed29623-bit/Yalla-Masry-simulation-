561c4b2ac3ce576c9f4190546eb95e6de8824620
adamahmed29623-bit <adamahmed29623@gmail.com>
2026-01-03 22:58:18 +0200

    Create index.tsx

---
+++ src/pages/adults/index.tsx
@@ -0,0 +1,12 @@
+    import PharaohAvatar from '../../components/PharaohAvatar';
+    import PharaohStory from '../../components/PharaohStory';
+
+    export default function AdultsPage() {
+      return (
+        <div>
+          <PharaohAvatar name="راميس" />
+          <PharaohStory />
+        </div>
+      );
+    }
+    
