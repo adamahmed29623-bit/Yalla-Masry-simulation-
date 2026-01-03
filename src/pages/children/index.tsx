9a786925105ece62fd6cf63b5e6a49162de016e4
adamahmed29623-bit <adamahmed29623@gmail.com>
2026-01-03 22:56:23 +0200

    Create index.tsx

---
+++ src/pages/children/index.tsx
@@ -0,0 +1,12 @@
+    import PharaohAvatar from '../../components/PharaohAvatar';
+    import PharaohGame from '../../components/PharaohGame';
+
+    export default function ChildrenPage() {
+      return (
+        <div>
+          <PharaohAvatar name="راميس" />
+          <PharaohGame />
+        </div>
+      );
+    }
+    
