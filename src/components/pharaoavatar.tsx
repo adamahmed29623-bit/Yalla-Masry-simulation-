02a4e3a09a271886aba9980db6e070f1b12464d0
adamahmed29623-bit <adamahmed29623@gmail.com>
2026-01-03 22:50:47 +0200

    Create pharaoavatar.tsx

---
+++ src/components/pharaoavatar.tsx
@@ -0,0 +1,9 @@
+    export default function PharaohAvatar({ name }) {
+      return (
+        <div>
+          <h3>{name}</h3>
+          <img src="/images/pharaoh-avatar.png" alt="Pharaoh Avatar" />
+        </div>
+      );
+    }
+    
