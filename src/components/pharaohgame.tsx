03335c6e84e3ebf8c17d6dcae34b45b697622c3b
adamahmed29623-bit <adamahmed29623@gmail.com>
2026-01-03 22:52:21 +0200

    Create pharaohgame.tsx

---
+++ src/components/pharaohgame.tsx
@@ -0,0 +1,9 @@
+    export default function PharaohGame() {
+      return (
+        <div>
+          <h3>لعبة الفراعنة</h3>
+          <!-- ألعاب تعليمية تفاعلية -->
+        </div>
+      );
+    }
+    
